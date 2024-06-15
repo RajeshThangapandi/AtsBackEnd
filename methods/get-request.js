6ee4ce3a44590451dbf5f29dbc4ae90f93a657a8
RajeshThangapandi <126014783+RajeshThangapandi@users.noreply.github.com>
2024-06-15 21:10:28 +0530

    Update get-request.js

--- methods/get-request.js
+++ methods/get-request.js
@@ -1,16 +1,19 @@
-module.exports = (req, res) => {
-  let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1);
-  let id = req.url.split("/")[3];
-  const regexV4 = new RegExp(
-    /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-  );
+const url = require('url');
 
-  if (req.url === "/applicant") {
+module.exports = (req, res) => {
+  let baseUrl = req.url;
+ 
+const parse= url.parse(req.url).pathname.split('/');
+const id =parseInt(parse[parse.length-1]);
+
+
+
+  if (baseUrl === "/applicant") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.write(JSON.stringify(req.movies));
     res.end();
-  } else if (!regexV4.test(id)) {
+  } else if (id==NaN) {
     res.writeHead(400, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
@@ -18,7 +21,7 @@ module.exports = (req, res) => {
         message: "UUID is not valid",
       })
     );
-  } else if (baseUrl === "/applicant" && regexV4.test(id)) {
+  } else if (baseUrl !== "/applicant") {
     res.setHeader("Content-Type", "application/json");
     let filteredMovie = req.movies.filter((movie) => {
       return movie.id === id;
